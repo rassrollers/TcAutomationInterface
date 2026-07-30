e04b3d759db5e2ada3846a71a357ae7e24f0325f
Rasmus Bertelsen <rasmusmbertelsen@gmail.com>
2026-07-30 18:23:17 +0200

    Update example code and usage

--- Example/Example.cs
+++ Example/Example.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using TwinCatBuildServerTool;
 
 string AppName = Assembly.GetExecutingAssembly().GetName().Name!;
 string AppVersion = Assembly.GetExecutingAssembly().GetName().Version!.ToString();
@@ -55,27 +54,39 @@ try
     using TcXaeShellEnvironment tcEnv = new(logger);
     await tcEnv.OpenEnvironment(options.SolutionPath);
     await tcEnv.ReloadMotionTasks();
-    tcEnv.InstallLibrariesFromDirectory(Path.Combine(options.WorkDir, "PlcProjecy\\_Libraries"));
+    tcEnv.InstallLibrariesFromDirectory(Path.Combine(options.WorkDir, "PlcProject\\_Libraries"));
+    
+    // Prepare TcUnit test environment
     tcEnv.SetProjectVariant("iTest");
+    tcEnv.SetupTcUnitTest(config["UnitTest:TcUnitResultPath"]);
+    
+    // Build and deploy the solution to the target PLC
     await tcEnv.CleanAndRebuildSolution();
+    tcEnv.ConfigureTarget(config);
+    tcEnv.DownloadToPlc();
+    tcEnv.StartTwinCat();
+    await Task.Delay(TimeSpan.FromSeconds(5));
+
+    logger.LogInformation(ads.GetTwinCatStatus());
+    if (!ads.IsTwinCatInRunMode())
+    {
+        logger.LogWarning("TwinCAT did not start after download. Trying to restart TwinCAT...");
+        ads.SetTwinCatInRunMode();
+        await Task.Delay(TimeSpan.FromSeconds(5));
+        if (!ads.IsTwinCatInRunMode())
+            throw new TwinCatException("TwinCAT system service is not in RUN mode after trying to restart");
+    }
+
+    // Wait for TcUnit tests to finish and print the results
+    tcEnv.WaitForUnitTestToDone();
 
     switch (options.Type)
     {
         case BuildType.Project:
-            tcEnv.ConfigureTarget(config);
-            tcEnv.DownloadToPlc();
-            tcEnv.StartTwinCat();
-            await Task.Delay(TimeSpan.FromSeconds(5));
+            break;
 
-            logger.LogInformation(ads.GetTwinCatStatus());
-            if (!ads.IsTwinCatInRunMode())
-            {
-                logger.LogWarning("TwinCAT did not start after download. Trying to restart TwinCAT...");
-                ads.SetTwinCatInRunMode();
-                await Task.Delay(TimeSpan.FromSeconds(5));
-                if (!ads.IsTwinCatInRunMode())
-                    throw new TwinCatException("TwinCAT system service is not in RUN mode after trying to restart");
-            }
+        case BuildType.Library:
+            tcEnv.SaveAsLibrary(".\\build\\", git.GetShortVersion());
             break;
 
         default:
@@ -83,73 +94,61 @@ try
             break;
     }
     logger.LogInformation("- - - - - Finished building: {type} - - - - -", options.Type.ToString());
-    return ExitCode.Success;
 }
 catch (WrongOSException ex)
 {
     logger.LogCritical("Not a Windows OS!");
     logger.LogDebug("{exMsg}", ex.Message);
-    return ExitCode.SystemError;
 }
 catch (LibGit2SharpException ex)
 {
     logger.LogCritical("{exMsg}", ex.Message);
     logger.LogDebug("No tag in repository");
-    return ExitCode.ArgumentError;
 }
 catch (ArgumentException ex)
 {
     logger.LogCritical("{exMsg}", ex.Message);
     logger.LogDebug("{ex}", ex.ToString());
-    return ExitCode.ArgumentError;
 }
 catch (TwinCatException ex)
 {
     logger.LogCritical("TwinCAT error: {exMsg}", ex.Message);
     logger.LogDebug("{ex}", ex.ToString());
-    return ExitCode.TwinCatError;
 }
 catch (AutomationInterfaceException ex)
 {
     logger.LogCritical("Automation Interface error: {exMsg}", ex.Message);
     logger.LogDebug("{ex}", ex.ToString());
-    return ExitCode.TwinCatError;
 }
 catch (TcXmlException ex)
 {
     logger.LogCritical("TcXml error: {exMsg}", ex.Message);
     logger.LogDebug("{ex}", ex.ToString());
-    return ExitCode.TwinCatError;
 }
 catch (UnauthorizedAccessException ex)
 {
     logger.LogCritical($"Access denied!");
     logger.LogDebug("{ex}", ex.ToString());
-    return ExitCode.TwinCatError;
 }
 catch (FileNotFoundException ex)
 {
     logger.LogCritical("{exMsg}", ex.Message);
     logger.LogDebug("{ex}", ex.ToString());
-    return ExitCode.SystemError;
 }
 catch (DirectoryNotFoundException ex)
 {
     logger.LogCritical("{exMsg}", ex.Message);
     logger.LogDebug("{ex}", ex.ToString());
-    return ExitCode.SystemError;
 }
 catch (COMException ex)
 {
     logger.LogCritical("COM exception: 0x{errorCode:X} - {message}", ex.ErrorCode, ex.Message);
     logger.LogDebug("{ex}", ex.ToString());
-    return ExitCode.ComObjectError;
 }
 catch (Exception ex)
 {
     logger.LogCritical("{exMsg}", ex.Message);
     logger.LogDebug("{ex}", ex.ToString());
-    return ExitCode.Unknown;
 }
 finally
 {
